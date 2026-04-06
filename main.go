d38d1b00a4063458e099c205e0a42219b7dcee0f
yumee2 <miyazaki1707@gmail.com>
2026-04-06 14:57:54 +0600

    feature: restore the file from the chunks and creating a separate dir for every file

--- main.go
+++ main.go
@@ -8,19 +8,21 @@ import (
 
 const chunkSizeKB = 500
 
+var rootDir, _ = os.Getwd()
+
 func main() {
 	rootDir, _ := os.Getwd()
 	path := filepath.Join(rootDir, "image.jpg")
 	data, _ := os.ReadFile(path)
 
-	fmt.Println(len(data), len(data)/1024, len(data)/(1024*1024))
+	os.Mkdir("image_jpg", 0755)
 
 	var chunk = make([]byte, 0, chunkSizeKB*1024)
 	var chunkIndex = 0
 	for _, b := range data {
 		chunk = append(chunk, b)
 		if len(chunk) >= chunkSizeKB*1024 {
-			chunkPath := filepath.Join(rootDir, fmt.Sprintf("chunk_%d", chunkIndex))
+			chunkPath := filepath.Join(rootDir, "image_jpg", fmt.Sprintf("chunk_%d", chunkIndex))
 			os.WriteFile(chunkPath, chunk, 0644)
 			chunkIndex++
 			chunk = make([]byte, 0, chunkSizeKB*1024)
@@ -28,7 +30,23 @@ func main() {
 	}
 
 	if len(chunk) > 0 {
-		chunkPath := filepath.Join(rootDir, fmt.Sprintf("chunk_%d", chunkIndex))
+		chunkPath := filepath.Join(rootDir, "image_jpg", fmt.Sprintf("chunk_%d", chunkIndex))
 		os.WriteFile(chunkPath, chunk, 0644)
 	}
+
+	restoreFile(filepath.Join(rootDir, "image_jpg"))
+}
+
+func restoreFile(fileDir string) {
+	var fileData []byte
+	entities, _ := os.ReadDir(fileDir)
+
+	for _, entity := range entities {
+		filePath := filepath.Join(fileDir, entity.Name())
+		chunkData, _ := os.ReadFile(filePath)
+		fileData = append(fileData, chunkData...)
+	}
+
+	resultFilePath := filepath.Join(rootDir, "restored_image.jpg")
+	os.WriteFile(resultFilePath, fileData, 0644)
 }
